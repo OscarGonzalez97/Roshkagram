02eb26eb1ff15b15c66a422c6e18fe7aa40e50c7
Diego <diegoyegrosa@gmail.com>
2022-12-30 23:22:02 -0300

    Deletion of unnecesary Files and Code formatting

--- src/main/java/com/roshka/bootcamp/ProyectoJunio/controller/dto/ComentarioDTO.java
+++ src/main/java/com/roshka/bootcamp/ProyectoJunio/controller/dto/ComentarioDTO.java
@@ -1,12 +1,7 @@
 package com.roshka.bootcamp.ProyectoJunio.controller.dto;
-
-import com.roshka.bootcamp.ProyectoJunio.model.Comentario;
 import com.roshka.bootcamp.ProyectoJunio.model.Foto;
 import com.roshka.bootcamp.ProyectoJunio.model.Usuario;
 import lombok.Data;
-
-import java.util.Date;
-
 @Data
 public class ComentarioDTO {
     private String descripcion;
@@ -15,16 +10,4 @@ public class ComentarioDTO {
     private Usuario usuario;
     private String idFoto;
     private String idComentario;
-    public ComentarioDTO(){
-
-    }
-    public ComentarioDTO (Comentario comentario){
-        this.descripcion = comentario.getDescripcion();
-        this.idComentario = String.valueOf(comentario.getId_comentario());
-//        this.foto = comentario.getFoto();
-        this.nombreCompleto = comentario.getComentarioUsuario().getNombre() + " " + comentario.getComentarioUsuario().getApellido();
-        this.usuario = comentario.getComentarioUsuario();
-        this.idFoto = String.valueOf(comentario.getFoto().getId_foto());
-    }
-
 }
--- src/main/java/com/roshka/bootcamp/ProyectoJunio/service/ComentarioService.java
+++ src/main/java/com/roshka/bootcamp/ProyectoJunio/service/ComentarioService.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class ComentarioService implements ComentarioServiceInterace {
+public class ComentarioService {
 
     @Autowired
     private ComentarioRepository comentarioRepository;
