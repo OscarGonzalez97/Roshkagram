307a647487d822c0ecac974cb7143c3c091d4d45
Diego <diegoyegrosa@gmail.com>
2022-12-31 12:01:28 -0300

    Parsed Date with timestamp to month,day,year and fixed ruta NoSuchElementException

--- src/main/java/com/roshka/bootcamp/ProyectoJunio/controller/dto/AlbumDTOAPI.java
+++ src/main/java/com/roshka/bootcamp/ProyectoJunio/controller/dto/AlbumDTOAPI.java
@@ -6,13 +6,14 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.*;
 
 @Data
 public class AlbumDTOAPI {
     private String titulo;
     private Long idAlbum;
-    private Date fechaCreacion;
+    private String fechaCreacion;
     private String nombreUsuario;
     private String categoria;
     private String ruta;
@@ -20,9 +21,21 @@ public class AlbumDTOAPI {
     public AlbumDTOAPI(Album album) {
         this.titulo = album.getTitulo();
         this.idAlbum = album.getId_album();
-        this.fechaCreacion = album.getFechaCreacion();
+        this.fechaCreacion = getDate(album);
         this.nombreUsuario = album.getUsuario().getNombre() + " " + album.getUsuario().getApellido();
         this.categoria = album.getCategoria().getNombre();
-        this.ruta = album.getFotos().stream().findAny().get().getRuta();
+        if (album.getFotos().stream().findAny().isPresent())
+            this.ruta = album.getFotos().stream().findAny().get().getRuta();
+        else
+            this.ruta = null;
+    }
+
+    public String getDate(Album album){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(album.getFechaCreacion());
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH)+1;
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        return day+"-"+month+"-"+year;
     }
 }
