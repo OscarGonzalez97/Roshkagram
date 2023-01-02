af63b0c61ab687f6ea845fc9184cc2e0c1c8be11
Elias Olmedo <elias.olmedor79@gmail.com>
2023-01-02 00:37:50 -0300

    "se agrega query para UsuarioDTO"

--- src/main/java/com/roshka/bootcamp/ProyectoJunio/controller/repository/UsuarioRepository.java
+++ src/main/java/com/roshka/bootcamp/ProyectoJunio/controller/repository/UsuarioRepository.java
@@ -1,13 +1,21 @@
 package com.roshka.bootcamp.ProyectoJunio.controller.repository;
 
+import com.roshka.bootcamp.ProyectoJunio.controller.dto.UsuarioDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.roshka.bootcamp.ProyectoJunio.model.Usuario;
+
+import java.util.List;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     public Usuario findByEmail(String email);
+
+    @Query(value = "SELECT new com.roshka.bootcamp.ProyectoJunio.controller.dto.UsuarioDTO(u.id_usuario , u.nombre, u.apellido) FROM Usuario u where u.id_usuario=:id")
+    List<UsuarioDTO> findAllUsuarioDTO(@Param("id") Long id);
 
 }
