9fd101d763b62ce84a4b2af056dc6d039fd01b4a
Elias Olmedo <elias.olmedor79@gmail.com>
2023-01-02 00:31:08 -0300

    "se agrega query para FotoDTO"

--- src/main/java/com/roshka/bootcamp/ProyectoJunio/controller/repository/FotoRepository.java
+++ src/main/java/com/roshka/bootcamp/ProyectoJunio/controller/repository/FotoRepository.java
@@ -1,8 +1,11 @@
 package com.roshka.bootcamp.ProyectoJunio.controller.repository;
 
+import com.roshka.bootcamp.ProyectoJunio.controller.dto.FotoDTO;
 import com.roshka.bootcamp.ProyectoJunio.model.Album;
 import com.roshka.bootcamp.ProyectoJunio.model.Foto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +13,8 @@ import java.util.List;
 @Repository
 public interface FotoRepository extends JpaRepository<Foto, Long> {
     List<Foto> findAllByAlbum(Album album);
+
+    @Query(value = "SELECT new com.roshka.bootcamp.ProyectoJunio.controller.dto.FotoDTO(f.id_foto , f.ruta , f.descripcion) FROM Foto f where f.id_foto=:id")
+    FotoDTO findByIdFotoDTO(@Param("id") Long id);
+
 }
