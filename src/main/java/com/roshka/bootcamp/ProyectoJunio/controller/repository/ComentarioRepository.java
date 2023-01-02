53d63eae2a26a86aaa47374827dc45e130fcfb42
Elias Olmedo <elias.olmedor79@gmail.com>
2023-01-02 01:31:42 -0300

    "se agrega query para ComentarioDTOAPI"

--- src/main/java/com/roshka/bootcamp/ProyectoJunio/controller/repository/ComentarioRepository.java
+++ src/main/java/com/roshka/bootcamp/ProyectoJunio/controller/repository/ComentarioRepository.java
@@ -1,8 +1,20 @@
 package com.roshka.bootcamp.ProyectoJunio.controller.repository;
 
+import com.roshka.bootcamp.ProyectoJunio.controller.dto.ComentarioDTOAPI;
 import com.roshka.bootcamp.ProyectoJunio.model.Comentario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ComentarioRepository extends JpaRepository<Comentario, Long>{ }
+public interface ComentarioRepository extends JpaRepository<Comentario, Long>{
+
+
+    public static final String query = "SELECT decripcion , usuario_id , id_comentario FROM Comentario where foto_id = :id";
+    @Query(value = query, nativeQuery = true)
+    List<ComentarioDTOAPI> findAllComentarioDTOAPIById(@Param("id") Long id);
+
+}
