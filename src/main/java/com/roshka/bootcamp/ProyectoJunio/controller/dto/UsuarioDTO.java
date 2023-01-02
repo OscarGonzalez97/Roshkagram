7558438753e7d4ff5bd30ef673f202ce39644dde
Elias Olmedo <elias.olmedor79@gmail.com>
2023-01-02 00:25:53 -0300

    "se agregan atributos a fotoDTO"

--- src/main/java/com/roshka/bootcamp/ProyectoJunio/controller/dto/UsuarioDTO.java
+++ src/main/java/com/roshka/bootcamp/ProyectoJunio/controller/dto/UsuarioDTO.java
@@ -1,2 +1,12 @@
-package com.roshka.bootcamp.ProyectoJunio.controller.dto;public class UsuarioDTO {
+package com.roshka.bootcamp.ProyectoJunio.controller.dto;
+
+import lombok.Data;
+
+@Data
+public class UsuarioDTO {
+    private Long id_usuario;
+    private String nombre;
+
+    private String apellido;
+
 }
