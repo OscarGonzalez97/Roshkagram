dfcb8fa7104bed91e7df30b3758b54830c498a98
Elias Olmedo <elias.olmedor79@gmail.com>
2023-01-03 13:55:02 -0300

    "se modifica AlbumControllerRest"

--- src/main/java/com/roshka/bootcamp/ProyectoJunio/controller/AlbumControllerRest.java
+++ src/main/java/com/roshka/bootcamp/ProyectoJunio/controller/AlbumControllerRest.java
@@ -1,30 +1,44 @@
 package com.roshka.bootcamp.ProyectoJunio.controller;
 
-import com.roshka.bootcamp.ProyectoJunio.controller.dto.AlbumDTO;
-import com.roshka.bootcamp.ProyectoJunio.model.Album;
-import com.roshka.bootcamp.ProyectoJunio.service.AlbumService;
+import com.roshka.bootcamp.ProyectoJunio.controller.dto.AlbumFotoDTO;
+import com.roshka.bootcamp.ProyectoJunio.controller.dto.AlbumDTOAPI;
+import com.roshka.bootcamp.ProyectoJunio.controller.dto.FotoDTOAPI;
+import com.roshka.bootcamp.ProyectoJunio.controller.repository.AlbumRepository;
+import com.roshka.bootcamp.ProyectoJunio.controller.repository.FotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping(value = "/API")
 public class AlbumControllerRest {
     @Autowired
-    private AlbumService albumService;
+    FotoRepository fotoRepository;
+    @Autowired
+    AlbumRepository albumRepository;
 
-    @GetMapping(value="/album-json/{id}",  produces = MediaType.APPLICATION_JSON_VALUE)
-    public AlbumDTO getAlbumJsonById(@PathVariable long id) throws Exception {
+    @GetMapping(value="/AlbumFoto/{id}",  produces = MediaType.APPLICATION_JSON_VALUE)
+    public AlbumFotoDTO getAlbumJsonById(@PathVariable long id) throws Exception {
+        AlbumDTOAPI album = null;
+        List<FotoDTOAPI> fotos = null;
+        try{
+            fotos = fotoRepository.findAllFotoDTOAPI(id);
+            album = albumRepository.findAlbumDTOAPI(id);
 
-        Optional<Album> album = albumService.findById(id);
-        AlbumDTO albumRetorno=null;
-
-        if(album.isPresent()){
-            albumRetorno = new AlbumDTO(album.get());
+            if(album != null){
+                return new AlbumFotoDTO(album, fotos);
+            }
+        }catch (Exception e){
+            System.err.println(e.getClass().getName() + e.getStackTrace());
+            return null;
         }
-        return albumRetorno;
+
+        return null;
     }
 }
